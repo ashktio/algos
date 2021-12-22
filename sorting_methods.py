3c63afc93b1cf44123ddf8a0dd38cb57b1446260
Ashenafi <ashenafi.teressa@gmail.com>
2021-12-21 20:19:02 -0800

    added python sorting methods

---
+++ sorting_methods.py
@@ -0,0 +1,32 @@
+arr = [19,5,-32,90,3,2,0,8]
+
+def bubbleSort(arr):
+  for j in range(len(arr) -1):
+    for i in range(len(arr) -1 - j):
+      if arr[i] > arr[i+1]:
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+  return arr
+print("bubble sorted", bubbleSort(arr))
+
+def selectionSort(arr):
+  for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+      if arr[min_idx] > arr[j]:
+        min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+  return arr
+print("selection sorted", selectionSort(arr))
+
+
+def insertionSort(arr):
+  for i in range(1, len(arr)):
+    key = arr[i]
+    j = i -1
+    while j >=0 and key < arr[j]:
+      arr[j], key = key,arr[j]
+    
+  for i in range(len(arr)):
+    print ("%d" %arr[i])
+  return arr
+print("printed with insertion sort", insertionSort(arr))
